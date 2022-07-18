c950a4a3dd9d050ef399db3c673265e90095305d
Prem Pavuluri <52355836+premkpavuluri@users.noreply.github.com>
2022-07-18 14:20:57 +0530

    Restricted the player from not entering the move from Front-End.

--- public/playGame.js
+++ public/playGame.js
@@ -36,20 +36,38 @@ const updateBoard = (xhr) => {
   xhrRequest(request, 200, renderBoard, identity);
 };
 
-const sendMove = (event) => {
+const sendMove = (move) => {
   const req = {
     method: 'POST', url: '/update-game',
     'content-type': 'application/json'
   };
-  const body = { move: +event.target.id };
+
+  const body = { move };
 
   xhrRequest(req, 201, updateBoard, identity, JSON.stringify(body));
 };
 
+const validateMove = (event) => {
+  const req = { method: 'GET', url: '/game-details' };
+  const move = +event.target.id;
+
+  const xhr = new XMLHttpRequest();
+  xhr.onload = () => {
+    const { isMyTurn } = JSON.parse(xhr.responseText);
+    if (isMyTurn) {
+      return sendMove(move);
+    }
+
+    console.log(isMyTurn);
+  };
+
+  xhr.open(req.method, req.url);
+  xhr.send();
+};
+
 const play = () => {
   const board = document.querySelector('.board');
-  board.onclick = sendMove;
+  board.onclick = validateMove;
 };
 
 window.onload = play;
-
--- src/handlers/gameDetails.js
+++ src/handlers/gameDetails.js
@@ -1,7 +1,11 @@
 const gameDetails = (gameInfo) => (req, res) => {
   const { game } = gameInfo;
+  const state = game.getState();
+  const { sessionId } = req.session;
+  const isMyTurn = sessionId === state.currentPlayer.id;
+  const { player1Moves, player2Moves, status } = state;
 
-  res.json(game.getState());
+  res.json({ player1Moves, player2Moves, isMyTurn, status });
 };
 
 module.exports = { gameDetails };
