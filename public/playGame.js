83b0e4fcee27839cc421e28ddefa0dde9f6413a2
Prem Pavuluri <52355836+premkpavuluri@users.noreply.github.com>
2022-07-18 11:25:41 +0530

    Rendering the board according to players moves.

--- public/playGame.js
+++ public/playGame.js
@@ -8,21 +8,42 @@ const xhrRequest = (request, onStatus, handler, altHandler, body = '') => {
 
     altHandler(xhr);
   }
+
+  const contentType = request['content-type'] || 'text/plain';
+
   xhr.open(request.method, request.url);
+  xhr.setRequestHeader('content-type', contentType);
   xhr.send(body);
 };
 
 const identity = (x) => x;
 
+const renderCells = (cells, symbol) => {
+  cells.forEach((cell) => {
+    const cellElement = document.getElementById(cell.toString());
+    cellElement.innerText = symbol;
+  });
+};
+
+const renderBoard = (xhr) => {
+  const { player1Moves, player2Moves } = JSON.parse(xhr.responseText);
+  renderCells(player1Moves, 'X');
+  renderCells(player2Moves, 'O');
+};
+
 const updateBoard = (xhr) => {
-  console.log(xhr);
+  const request = { method: 'GET', url: '/game-details' };
+  xhrRequest(request, 200, renderBoard, identity);
 };
 
 const sendMove = (event) => {
-  const req = { method: 'POST', url: '/update-game' }
-  const body = { move: event.target.id };
+  const req = {
+    method: 'POST', url: '/update-game',
+    'content-type': 'application/json'
+  };
+  const body = { move: +event.target.id };
 
-  xhrRequest(req, 201, updateBoard, identity, body);
+  xhrRequest(req, 201, updateBoard, identity, JSON.stringify(body));
 };
 
 const play = () => {
@@ -31,3 +52,4 @@ const play = () => {
 };
 
 window.onload = play;
+
